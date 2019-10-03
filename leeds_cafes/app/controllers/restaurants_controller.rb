95f850506e6112e6005494054b24fbc423074bc1
JennicaStiehl <jennica.stiehl@gmail.com>
2019-10-03 15:43:27 -0600

    Adds index action, and removes category and post_cost actions.

--- leeds_cafes/app/controllers/restaurants_controller.rb
+++ leeds_cafes/app/controllers/restaurants_controller.rb
@@ -1,10 +1,21 @@
 class RestaurantsController < ApplicationController
+
   def index
-    @title = params["post_code"]
-    @cafes = Restaurant.find_by_post_code(@title)
-    @total_chairs = Restaurant.total_chairs(@title)
-    @total_places = Restaurant.total_places(@title)
-    @chairs_pct = Restaurant.chairs_pct(@title)
-    @max_chairs = Restaurant.max_chairs(@title)
+    @filter_by = params["filter_by"]
+    @title = params["value"]
+    @cafes = Restaurant.find_by_filter(@title, @filter_by)
+    @total_chairs = Restaurant.total_chairs(@title, @filter_by)
+    @total_places = Restaurant.total_places(@title, @filter_by)
+    @chairs_pct = Restaurant.chairs_pct(@title, @filter_by)
+    @max_chairs = Restaurant.max_chairs(@title, @filter_by)
+    # elsif params["filter_by"] == 'category'
+    #   @title = params["value"]
+    #   @cafes = Restaurant.find_by_filter(@title, 'category')
+    #   @total_chairs = Restaurant.total_chairs(@title, 'category')
+    #   @total_places = Restaurant.total_places(@title, 'category')
+    #   @chairs_pct = Restaurant.chairs_pct(@title, 'category')
+    #   @max_chairs = Restaurant.max_chairs(@title, 'category')
+    # end
   end
+
 end
