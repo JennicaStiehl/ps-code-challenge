68d5c28cac59e1e87bc756fa7fb5a4d0f6fff7ff
JennicaStiehl <jennica.stiehl@gmail.com>
2019-10-04 15:00:49 -0600

    Cleans up comments, adds a gem.

--- leeds_cafes/app/controllers/restaurants_controller.rb
+++ leeds_cafes/app/controllers/restaurants_controller.rb
@@ -8,14 +8,6 @@ class RestaurantsController < ApplicationController
     @total_places = Restaurant.total_places(@title, @filter_by)
     @chairs_pct = Restaurant.chairs_pct(@title, @filter_by)
     @max_chairs = Restaurant.max_chairs(@title, @filter_by)
-    # elsif params["filter_by"] == 'category'
-    #   @title = params["value"]
-    #   @cafes = Restaurant.find_by_filter(@title, 'category')
-    #   @total_chairs = Restaurant.total_chairs(@title, 'category')
-    #   @total_places = Restaurant.total_places(@title, 'category')
-    #   @chairs_pct = Restaurant.chairs_pct(@title, 'category')
-    #   @max_chairs = Restaurant.max_chairs(@title, 'category')
-    # end
   end
 
 end
--- leeds_cafes/config/routes.rb
+++ leeds_cafes/config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   get '/restaurants/:filter_by/:value', to: 'restaurants#index'
-  # get '/restaurants/category/:category', to: 'restaurants#index', as: 'category'
 end
