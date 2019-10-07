4fd7e9ac4ad1823022c89109f69a299c84ee2e01
JennicaStiehl <jennica.stiehl@gmail.com>
2019-10-07 09:11:39 -0600

    Adds scenic and SQL view for post codes

---
+++ leeds_cafes/app/models/post_code.rb
@@ -0,0 +1,13 @@
+class PostCode < ApplicationRecord
+  belongs_to :data
+  self.primary_key = 'pc_id'
+
+  def self.refresh
+    Scenic.database.refresh_materialized_view(:post_codes, concurrently: false, cascade: false)
+  end
+
+  private
+  def readonly?
+    true
+  end
+end
---
+++ leeds_cafes/db/views/post_codes_v01.sql
@@ -0,0 +1,10 @@
+SELECT post_code
+              , id as "pc id"
+              , max(number_of_chairs)as "Most Chairs"
+              , sum(number_of_chairs)as "Total Chairs"
+              , count(id)as "Total Places"
+              , (SELECT sum(number_of_chairs) FROM restaurants)as "Total Chairs in Leeds"
+              , (sum(number_of_chairs) / (SELECT sum(number_of_chairs) FROM restaurants))as "Percent Leeds Chairs"
+FROM restaurants
+GROUP BY post_code
+ORDER BY post_code;
